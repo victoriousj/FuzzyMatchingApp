77e137c48ec642336d06bbea2a3f693afe32d437
VictoriousJ <victordjohnson@gmail.com>
2018-03-09 19:39:35 -0600

    Added the context class as well as the Customer Model, oh, and Entity Framework.

---
+++ FuzzyMatchingApp/Context.cs
@@ -0,0 +1,14 @@
+﻿using FuzzyMatchingApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FuzzyMatchingApp
+{
+	public class Context: DbContext
+	{
+		public DbSet<Customer> Customers { get; set; }
+	}
+}
---
+++ FuzzyMatchingApp/Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FuzzyMatchingApp.Models
+{
+	public class Customer
+	{
+		public Customer() { }
+
+		public Customer(string firstName, string middleName, string lastName)
+		{
+			FirstName = firstName;
+			MiddleName = middleName;
+			LastName = lastName;
+		}
+
+		public int ID { get; set; }
+
+		public string FirstName { get; set; }
+
+		public string MiddleName { get; set; }
+
+		public string LastName { get; set; }
+	}
+}
